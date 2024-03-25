348c3d3ffc7a7fbff9ef2c1100468f60532fe8d1
Tehseen Ali <154703743+Tehseenali445@users.noreply.github.com>
2024-03-26 00:16:22 +0500

    Math Expressions

---
+++ app.js
@@ -0,0 +1,237 @@
+// // Q1:
+
+// let num1 = 3;
+// let num2 = 5;
+
+// let sum = num1 + num2;
+
+// document.write(" Sum of " , num1 , " and " , num2 , " is " , sum , "<br/>" ,  "<hr/>" , "<br/>");
+
+// // Q2:
+// // Subtraction:
+
+// let num3 = 3;
+// let num4 = 5;
+
+// let sub = num3 - num4;
+
+// document.write(" Subtraction of Numbers " , num3 , " and " , num4 , " is " , sub , "<br/>" , "<hr/>" , "<br/>" );
+
+// // Multiplication;
+
+// let num5 = 3;
+// let num6 = 5;
+
+// let mul = num5 * num6;
+
+// document.write(" Multiplication of Number " , num5 , " and " , num6 , " is " , mul , "<br/>" ,  "<hr/>" , "<br/>");
+
+// // Division:
+
+// let num7 = 3;
+// let num8 = 5;
+
+// let div = num5 / num6;
+
+// document.write(" Division of Number " , num7 , " and " , num8 , " is " , mul , "<br/>" ,  "<hr/>" , "<br/>");
+
+// // Modulus:
+
+// let num9 = 3;
+// let num10 = 5;
+
+// let remainder  = num5 % num6;
+
+// document.write(" Modulus of Number " , num9 , " and " , num10 , " is " , remainder , "<br/>" ,  "<hr/>" , "<br/>");
+
+// Q3:
+
+// let apple =
+// document.write(" Value after variable declaration is undefind " , "<br/>" , "<br/>");
+
+// let banana = 5;
+// document.write("Initial Value:" , banana , "<br/>" , "<br/>");
+
+// let increment = ++banana;
+// let add = 7;
+
+// document.write(" Value after increment is: " , increment , "<br/>" , "<br/>");
+
+// let sum = (increment + add);
+// document.write(" Value after addition is: " , sum, "<br/>" , "<br/>");
+
+// let decrement = --sum;
+// document.write(" Value after Decrement is: " , decrement , "<br/>" , "<br/>");
+
+// let remainder = decrement % 3;
+// document.write(" The reminder is: " , remainder , "<br/>" , "<br/>");
+
+// Q4:
+
+// let movie_cost = 600;
+// let buying = 5;
+// let total = movie_cost * buying;
+
+// document.write(" Total cost os buy " , buying , " tickets to a movie is " , total, "PKR");
+
+// Q5:
+
+// let table = +prompt("Please enter your Table Number")
+
+// let num1 = table * 1;
+// let num2 = table * 2;
+// let num3 = table * 3;
+// let num4 = table * 4;
+// let num5 = table * 5;
+// let num6 = table * 6;
+// let num7 = table * 7;
+// let num8 = table * 8;
+// let num9 = table * 9;
+// let num10 = table * 10;
+
+// document.write("<h3/>" , table , " x " , " 1 " , " = " , num1 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 2 " , " = " , num2 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 3 " , " = " , num3 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 4 " , " = " , num4 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 5 " , " = " , num5 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 6 " , " = " , num6 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 7 " , " = " , num7 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 8 " , " = " , num8 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 9 " , " = " , num9 , "<br/>" , "<br/>");
+// document.write(table , " x " , " 10 " , " = " , num10 , "<br/>" , "<br/>");
+
+// Q6:
+
+// let Celsius = 25;
+// let A = 9;
+// let B = 5;
+// let Add = 32;
+
+// let sum = Celsius * A / B + Add;
+
+// document.write(Celsius , '<sup>o</sup>' , "C" , " is " , sum , '<sup>o</sup>' , "F" , "<br/>" , "<br/>")
+
+// let Fahrenheit = 70;
+// let a = 32;
+// let b = 5;
+// let add = 9;
+
+// let Sum = (Fahrenheit - a) * b / add;
+
+// document.write(Fahrenheit , '<sup>o</sup>' , "F" , " is " , Sum , '<sup>o</sup>' , "C")
+
+// Q7:
+
+// let Price_item1 = 650;
+// let Qty_item1 = 3;
+
+// let mul_1 = Price_item1 * Qty_item1;
+
+// let Price_item2 = 100;
+// let Qty_item2 = 7;
+
+// let mul_2 = Price_item2 * Qty_item2;
+
+// let shipping_charges = 100;
+
+// let total_cost = mul_1 + mul_2 + shipping_charges;
+// document.write(
+//   "<h3 style= 'font-size: 40px;'> Shoping Cart.</h3>",
+//   "<br/>",
+//   "<br/>"
+// );
+// document.write(
+//   " Price of item 1 is ",
+//   Price_item1,
+//   "<br/>",
+//   " Quantity of item 1 is ",
+//   Qty_item1,
+//   "<br/>",
+//   " Price of item 2 is ",
+//   Price_item2,
+//   "<br/>",
+//   " Quantity os item 2 is ",
+//   Qty_item2,
+//   "<br/>",
+//   " Shipping charges ",
+//   shipping_charges,
+//   "<br/>",
+//   "<br/>",
+//   "<br/>",
+//   " Total cost of your order is ",
+//   "<b>",
+//   total_cost
+// );
+
+// Q8:
+
+// let total_marks = +prompt("Enter your total Marks:");
+// let marks_obt = +prompt("Enter your total Marks Obtained:");
+// let percentage = marks_obt / total_marks * 100;
+
+// document.write("<h3 style= 'font-size: 40px;'> Marks Sheet</h3>",
+//   "<br/>",
+//   "<br/>")
+
+// document.write(" Total marks:" , total_marks , "<br/>");
+// document.write(" Marks Obtained: " , marks_obt , "<br/>");
+// document.write(" Percentage: " , percentage , "%" , "<br/>");
+
+// Q9:
+
+// let dollar = prompt("US Dollar is...");
+// let riyals = prompt("Saudi Riyals is...");
+
+// let dollar_in_pkr = 104.80;
+// let saudi_riyals_in_pkr = 28;
+
+// let convert_us = dollar * dollar_in_pkr;
+// let convert_riyals = riyals * saudi_riyals_in_pkr;
+
+// let total_amount = +convert_us + convert_riyals;
+
+// document.write("<h3 style= 'font-size: 40px;'> Currency in PKR</h3>",
+//   "<br/>",
+//   "<br/>")
+
+// document.write(" Total Currency in PKR: " , total_amount);
+
+// Q10:
+
+// let sequence = (4 + 5) * 10 / 2;
+// console.log(sequence);
+
+// Q11:
+
+// let current_year = +prompt("Emter the Current Year:")
+// let birth_year = +prompt("Enter your Birth year:");
+// let age = current_year - birth_year;
+
+// document.write(" Current Year:" , current_year , "<br/>");
+// document.write(" Birth Year:" , birth_year , "<br/>");
+// document.write(" Your Age is:" , age , "<br/>");
+
+// Q11:
+
+document.write(
+  "<h3 style= 'font-size: 40px;'> The Lifetime Supply Calculator</h3>",
+  "<br/>",
+  "<br/>"
+);
+
+let fav_food = "Finger Chips";
+let current_age = 28;
+let estimated_max_age = 80;
+let food_perday = 2;
+let ans = estimated_max_age - current_age * 365 * food_perday;
+console.log(ans);
+document.write(" Favourite Food: ", fav_food, "<br/>");
+document.write(" Current Age: ", current_age, "<br/>");
+document.write(" Estimated Maximum Age: ", estimated_max_age, "<br/>");
+document.write(" Amount of Food per day: ", food_perday, "<br/>");
+document.write(
+  " You will need: ",
+  ans,
+  " Finger Chips to last you until the ripe old age of 65 ",
+  "<br/>"
+);
